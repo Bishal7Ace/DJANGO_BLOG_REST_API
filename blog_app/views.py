1dfe899b9ae47a38aaf26f4c88d69cfe62a89caa
Ace <tamangbishal99@gmail.com>
2024-04-02 18:44:59 +0545

    initial:changes

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import *
 # Create your views here.
 
 def blog_list(request):
-    data = {
-        "Message":"Hello World"
-    }
+    blogs = Blog.objects.all()
     
+    data = {
+        "Blogs" : list(blogs.values())
+    }
+    return JsonResponse(data)
+
+def blog_detail(request, pk):
+    blogs = Blog.objects.get(pk=pk)
+    
+    data = {
+        "name" : blogs.name,
+        "description" : blogs.description,
+        "slug" : blogs.slug,
+    }
     return JsonResponse(data)
