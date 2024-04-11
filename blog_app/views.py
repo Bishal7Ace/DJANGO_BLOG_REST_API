00326d93c9271edf55d1375295a0bdd5873c1cec
Ace <tamangbishal99@gmail.com>
2024-04-11 19:01:38 +0545

    classbasedAPI

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,16 +5,17 @@ from .serializer import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+
+from rest_framework.views import APIView
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def blog_list(request):
-    if request.method == "GET":
-       all_blogs = Blog.objects.filter(is_public = True)
-       serializer = BlogSerializer(all_blogs, many=True)
-       return Response(serializer.data, status=status.HTTP_200_OK)
-   
-    if request.method == "POST":
+class BlogListView(APIView):
+    def get(self, request):
+        all_blogs = Blog.objects.filter(is_public = True)
+        serializer = BlogSerializer(all_blogs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
            serializer.save()
@@ -22,14 +23,15 @@ def blog_list(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','POST', 'DELETE'])
-def blog_detail(request, pk):
-    if request.method == "GET":
+
+#GET, PUT, DELETE
+class BlogDetailView(APIView):
+    def get(self, request, pk):
         blog = Blog.objects.get(is_public = True, pk=pk)
         serializer = BlogSerializer(blog)
         return Response(serializer.data)
     
-    if request.method == "POST":
+    def put(self, request, pk):
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog, data=request.data)
         if serializer.is_valid():
@@ -37,8 +39,44 @@ def blog_detail(request, pk):
            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-    if request.method == "DELETE":
+    
+    def delete(self, request, pk):
         blog = Blog.objects.get(pk=pk)
         blog.delete()
         return Response(status=status.HTTP_200_OK)
+        
+# @api_view(['GET', 'POST'])
+# def blog_list(request):
+#     if request.method == "GET":
+#        all_blogs = Blog.objects.filter(is_public = True)
+#        serializer = BlogSerializer(all_blogs, many=True)
+#        return Response(serializer.data, status=status.HTTP_200_OK)
+   
+#     if request.method == "POST":
+#         serializer = BlogSerializer(data=request.data)
+#         if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET','POST', 'DELETE'])
+# def blog_detail(request, pk):
+#     if request.method == "GET":
+#         blog = Blog.objects.get(is_public = True, pk=pk)
+#         serializer = BlogSerializer(blog)
+#         return Response(serializer.data)
+    
+#     if request.method == "POST":
+#         blog = Blog.objects.get(pk=pk)
+#         serializer = BlogSerializer(blog, data=request.data)
+#         if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+#     if request.method == "DELETE":
+#         blog = Blog.objects.get(pk=pk)
+#         blog.delete()
+#         return Response(status=status.HTTP_200_OK)
